00995c57c69bc1bd0efd751940d6fd6098090a5a
Danielle Demkiw <ddemkiw@gmail.com>
2015-04-09 14:57:45 +0100

    adding initial score

--- js/game.js
+++ js/game.js
@@ -1,5 +1,15 @@
-var Game = function (){};
+var Game = function (){
+  this.rounds= [];
+};
 
-Game.prototype.method_name = function(first_argument) {
-  // body...
+Game.prototype.startingScore = function() {
+  var self = this; 
+
+  for(i=0; i< 10; i++){
+    self.rounds.push({
+      score1:0,
+      score2:0
+    });
+  }
+  return this.rounds;
 };
--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,1 +1,17 @@
-gameSpec.js
+describe('Game', function(){
+
+  var game
+
+  beforeEach(function() { 
+    game = new Game();
+  });
+
+  describe('starting the game', function(){
+
+    it('it should have a score of 0', function(){
+      game.startingScore();
+      expect(game.rounds).toEqual([ { score1: 0, score2: 0 }, { score1: 0, score2: 0 }, { score1: 0, score2: 0 }, { score1: 0, score2: 0 }, { score1: 0, score2: 0 }, { score1: 0, score2: 0 }, { score1: 0, score2: 0 }, { score1: 0, score2: 0 }, { score1: 0, score2: 0 }, { score1: 0, score2: 0 } ]);
+    });
+  });
+
+});
